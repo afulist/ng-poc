a9821e7cb188ddf23095ce9dd5836fe3dd755d8d
afulist <afulist@cht.com.tw>
2014-03-21 13:59:46 +0800

    proj init

---
+++ app/scripts/controllers/testuser.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('travelApp')
+  .controller('TestuserCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+  
